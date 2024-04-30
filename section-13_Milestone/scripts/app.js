b19ad1e0b1221be67d71abdeec9884a47871db1f
Artur Tabor <148994551+Zdartyy@users.noreply.github.com>
2024-04-30 14:53:47 +0200

    Update app.js

--- section-13_Milestone/scripts/app.js
+++ section-13_Milestone/scripts/app.js
@@ -1,5 +1,13 @@
 let editedPlayer = 0;
 let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
+
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
 
 const players = [
     {
@@ -19,12 +27,14 @@ const formElement = document.querySelector('form');
 const error = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
 const activePlayerElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
 const startNewGameBtnElement = document.getElementById('start-new-game');
 const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
